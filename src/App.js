879df7a6f8e5115c14573931402c66a840c3d332
Hakan <hakan.gul@daimia.com>
2022-11-09 13:41:19 +0300

    user-counter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import User from "./components/User";
+import Counter from "./components/Counter";
+
 
 
 function App() {
   return (
-
     <div>
-      <User name="hakan gül" city={"Aksaray"} age={32}/> 
-      <User name="ahmet aslan" city={"ankara"} age={25}/>
-      <User name="memati baş" city={"istanbul"} age={38}/>      
-      <User name="polat alemdar" city={"yalova"} age={45}/>      
 
-
+      <Counter />
+      {/* <User         
+          data= {{
+          name: "hakan gül",
+          city: "aksaray",
+          age: 34,
+        }}
+        friends={["ahmet","hakan","murat","ayşe"]}
+      /> */}
     </div>
-
-
-    
   );
 }
 
--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function User(props) {
-    console.log(props);
+function User({ title, data, friends }) {
   return (
     <div>
-      User
-      <div>isim: {props.name}</div>
-      <div>yaş: {props.city}</div>
-      <div>şehir: {props.age}</div>
-
+      <h2>{title}</h2>
+      <div>isim: {data.name}</div>
+      <div>yaş: {data.age}</div>
+      <div>şehir: {data.city}</div>
+      <h3>Arkadaşlar</h3>
+      {friends.map((friend, i) => (
+        <div key={i}>{friend}</div>
+      ))}
       <hr />
     </div>
   );
 }
+
+User.propTypes = {
+  title: PropTypes.string.isRequired,
+  data: PropTypes.exact({
+    name: PropTypes.string,
+    age: PropTypes.number,
+    city: PropTypes.string,
+  }),
+  friends: PropTypes.array,
+};
+
+User.defaultProps = {
+  title: "Kişi",
+};
 
 export default User;
